095035012f2d5c029e7ebc5c41e7ecbfdc54aa3b
Thomas Culino <thomas.culino@gmail.com>
2026-01-26 17:16:24 -0800

    Update crossmatch catalogs

--- static/js/constants/crossmatch.js
+++ static/js/constants/crossmatch.js
@@ -1,3 +1,5 @@
+import { type } from "os";
+
 export const ztf_crossmatch_fields = {
   name: "cross_matches",
   type: {
@@ -7,89 +9,16 @@ export const ztf_crossmatch_fields = {
       name: "CrossMatch",
       fields: [
         {
-          name: "AllWISE",
+          name: "Gaia_DR3",
           type: [
             "null",
             {
               type: "record",
-              name: "AllWISEMatch",
+              name: "Gaia_DR3Match",
               fields: [
                 {
                   name: "_id",
                   type: "double",
-                },
-                {
-                  name: "w1mpro",
-                  type: "double",
-                },
-                {
-                  name: "w1sigmpro",
-                  type: "double",
-                },
-                {
-                  name: "w2mpro",
-                  type: "double",
-                },
-                {
-                  name: "w2sigmpro",
-                  type: "double",
-                },
-                {
-                  name: "w3mpro",
-                  type: "double",
-                },
-                {
-                  name: "w3sigmpro",
-                  type: "double",
-                },
-                {
-                  name: "w4mpro",
-                  type: "double",
-                },
-                {
-                  name: "w4sigmpro",
-                  type: "double",
-                },
-                {
-                  name: "ph_qual",
-                  type: "string",
-                },
-                {
-                  name: "coordinates",
-                  type: {
-                    type: "record",
-                    name: "WiseCoordinates",
-                    fields: [
-                      {
-                        name: "radec_str",
-                        type: {
-                          type: "array",
-                          items: "double",
-                        },
-                      },
-                    ],
-                  },
-                },
-              ],
-            },
-          ],
-          default: null,
-        },
-        {
-          name: "CLU_20190625",
-          type: [
-            "null",
-            {
-              type: "record",
-              name: "CLUEntry",
-              fields: [
-                {
-                  name: "_id",
-                  type: "double",
-                },
-                {
-                  name: "name",
-                  type: "string",
                 },
                 {
                   name: "ra",
@@ -100,56 +29,64 @@ export const ztf_crossmatch_fields = {
                   type: "double",
                 },
                 {
-                  name: "z",
+                  name: "parallax",
                   type: "double",
                 },
                 {
-                  name: "a",
+                  name: "parallax_error",
                   type: "double",
                 },
                 {
-                  name: "b2a",
+                  name: "pm",
+                  type: "float",
+                },
+                {
+                  name: "pmra",
+                  type: "float",
+                },
+                {
+                  name: "pmra_error",
+                  type: "float",
+                },
+                {
+                  name: "pmdec",
+                  type: "float",
+                },
+                {
+                  name: "pmdec_error",
+                  type: "float",
+                },
+                {
+                  name: "phot_g_mean_mag",
                   type: "double",
                 },
                 {
-                  name: "pa",
+                  name: "phot_bp_mean_mag",
                   type: "double",
                 },
                 {
-                  name: "sfr_ha",
+                  name: "phot_rp_mean_mag",
                   type: "double",
                 },
                 {
-                  name: "sfr_fuv",
-                  type: "double",
+                  name: "phot_g_n_obs",
+                  type: "int",
                 },
                 {
-                  name: "mstar",
-                  type: "double",
+                  name: "phot_bp_n_obs",
+                  type: "int",
                 },
                 {
-                  name: "coordinates",
-                  type: {
-                    type: "record",
-                    name: "CLUCoordinates",
-                    fields: [
-                      {
-                        name: "radec_str",
-                        type: {
-                          type: "array",
-                          items: "double",
-                        },
-                      },
-                      {
-                        name: "distance_arcsec",
-                        type: "double",
-                      },
-                      {
-                        name: "distance_kpc",
-                        type: "double",
-                      },
-                    ],
-                  },
+                  name: "phot_rp_n_obs",
+                  type: "int",
+                },
+                {
+                  name: "ruwe",
+                  type: "float",
+                },
+                {
+                  name: "phot_bp_rp_excess_factor",
+                  type: "float",
                 },
               ],
             },
@@ -157,12 +94,49 @@ export const ztf_crossmatch_fields = {
           default: null,
         },
         {
-          name: "NED_BetaV3",
+          name: "milliquas_v8",
           type: [
             "null",
             {
               type: "record",
-              name: "NED_BetaV3Match",
+              name: "Milliquas_v8Match",
+              fields: [
+                {
+                  name: "_id",
+                  type: "double",
+                },
+                {
+                  name: "Name",
+                  type: "string",
+                },
+                {
+                  name: "Descrip",
+                  type: "string",
+                },
+                {
+                  name: "Qpct",
+                  type: "float",
+                },
+                {
+                  name: "ra",
+                  type: "double",
+                },
+                {
+                  name: "dec",
+                  type: "double",
+                },
+              ],
+            },
+          ],
+          default: null,
+        },
+        {
+          name: "NED",
+          type: [
+            "null",
+            {
+              type: "record",
+              name: "NEDMatch",
               fields: [
                 {
                   name: "_id",
@@ -225,28 +199,12 @@ export const ztf_crossmatch_fields = {
                   type: "string",
                 },
                 {
-                  name: "coordinates",
-                  type: {
-                    type: "record",
-                    name: "Coordinates",
-                    fields: [
-                      {
-                        name: "radec_str",
-                        type: {
-                          type: "array",
-                          items: "string",
-                        },
-                      },
-                      {
-                        name: "distance_arcsec",
-                        type: "double",
-                      },
-                      {
-                        name: "distance_kpc",
-                        type: "double",
-                      },
-                    ],
-                  },
+                  name: "Mstar",
+                  type: "float",
+                },
+                {
+                  name: "Mstar_unc",
+                  type: "float",
                 },
               ],
             },
@@ -254,81 +212,226 @@ export const ztf_crossmatch_fields = {
           default: null,
         },
         {
-          name: "Gaia_EDR3",
+          name: "2MASS_PSC",
           type: [
             "null",
             {
               type: "record",
-              name: "Gaia_EDR3Match",
+              name: "TwoMASS_PSCMatch",
               fields: [
                 {
                   name: "_id",
                   type: "double",
                 },
                 {
-                  name: "parallax",
+                  name: "ra",
                   type: "double",
                 },
                 {
-                  name: "parallax_error",
+                  name: "dec",
                   type: "double",
                 },
                 {
-                  name: "phot_g_mean_mag",
+                  name: "j_m",
                   type: "double",
                 },
                 {
-                  name: "phot_bp_mean_mag",
+                  name: "j_msigcom",
+                  type: "float",
+                },
+                {
+                  name: "j_snr",
+                  type: "float",
+                },
+                {
+                  name: "h_m",
                   type: "double",
                 },
                 {
-                  name: "phot_rp_mean_mag",
+                  name: "h_msigcom",
+                  type: "float",
+                },
+                {
+                  name: "h_snr",
+                  type: "float",
+                },
+                {
+                  name: "k_m",
                   type: "double",
                 },
                 {
-                  name: "coordinates",
-                  type: {
-                    type: "record",
-                    name: "Coordinates",
-                    fields: [
-                      {
-                        name: "radec_str",
-                        type: {
-                          type: "array",
-                          items: "string",
-                        },
-                      },
-                    ],
-                  },
+                  name: "k_msigcom",
+                  type: "float",
+                },
+                {
+                  name: "k_snr",
+                  type: "float",
+                },
+                {
+                  name: "ndet",
+                  type: "int",
                 },
               ],
             },
           ],
           default: null,
         },
-      ],
-    },
-  },
-};
-
-export const lsst_crossmatch_fields = {
-  name: "cross_matches",
-  type: {
-    type: "array",
-    items: {
-      type: "record",
-      name: "CrossMatch",
-      fields: [
         {
-          name: "AllWISE",
+          name: "GALEX",
           type: [
             "null",
             {
               type: "record",
-              name: "AllWISEMatch",
+              name: "GALEXMatch",
               fields: [
                 {
                   name: "_id",
+                  type: "double",
+                },
+                {
+                  name: "ra",
+                  type: "double",
+                },
+                {
+                  name: "dec",
+                  type: "double",
+                },
+                {
+                  name: "fuv_mag",
+                  type: "float",
+                },
+                {
+                  name: "fuv_magerr",
+                  type: "float",
+                },
+                {
+                  name: "nuv_mag",
+                  type: "float",
+                },
+                {
+                  name: "nuv_magerr",
+                  type: "float",
+                },
+                {
+                  name: "fuv_exp",
+                  type: "float",
+                },
+                {
+                  name: "nuv_exp",
+                  type: "float",
+                },
+                {
+                  name: "obstype",
+                  type: "int",
+                },
+                {
+                  name: "band",
+                  type: "int",
+                },
+              ],
+            },
+          ],
+          default: null,
+        },
+        {
+          name: "VSX",
+          type: [
+            "null",
+            {
+              type: "record",
+              name: "VSXMatch",
+              fields: [
+                {
+                  name: "_id",
+                  type: "double",
+                },
+                {
+                  name: "name",
+                  type: "string",
+                },
+                {
+                  name: "var_flag",
+                  type: "int",
+                },
+                {
+                  name: "ra",
+                  type: "double",
+                },
+                {
+                  name: "dec",
+                  type: "double",
+                },
+                {
+                  name: "types",
+                  type: "string",
+                },
+                {
+                  name: "max",
+                  type: "double",
+                },
+                {
+                  name: "max_band",
+                  type: "string",
+                },
+                {
+                  name: "min_is_amplitude",
+                  type: "boolean",
+                },
+                {
+                  name: "min",
+                  type: "double",
+                },
+                {
+                  name: "min_band",
+                  type: "string",
+                },
+                {
+                  name: "epoch",
+                  type: "double",
+                },
+                {
+                  name: "period",
+                  type: "double",
+                },
+                {
+                  name: "spectral_type",
+                  type: "string",
+                },
+              ],
+            },
+          ],
+          default: null,
+        },
+        {
+          name: "CatWISE2020",
+          type: [
+            "null",
+            {
+              type: "record",
+              name: "CatWISE2020Match",
+              fields: [
+                {
+                  name: "_id",
+                  type: "double",
+                },
+                {
+                  name: "source_name",
+                  type: "string",
+                },
+                {
+                  name: "ra",
+                  type: "double",
+                },
+                {
+                  name: "dec",
+                  type: "double",
+                },
+                {
+                  name: "sigra",
+                  type: "double",
+                },
+                {
+                  name: "sigdec",
                   type: "double",
                 },
                 {
@@ -348,46 +451,51 @@ export const lsst_crossmatch_fields = {
                   type: "double",
                 },
                 {
-                  name: "w3mpro",
+                  name: "w1rchi2",
                   type: "double",
                 },
                 {
-                  name: "w3sigmpro",
+                  name: "w2rchi2",
                   type: "double",
                 },
                 {
-                  name: "w4mpro",
+                  name: "pmra",
                   type: "double",
                 },
                 {
-                  name: "w4sigmpro",
+                  name: "pmdec",
                   type: "double",
                 },
                 {
-                  name: "ph_qual",
+                  name: "sigpmra",
+                  type: "double",
+                },
+                {
+                  name: "sigpmdec",
+                  type: "double",
+                },
+                {
+                  name: "unwise_objid",
                   type: "string",
-                },
-                {
-                  name: "coordinates",
-                  type: {
-                    type: "record",
-                    name: "WiseCoordinates",
-                    fields: [
-                      {
-                        name: "radec_str",
-                        type: {
-                          type: "array",
-                          items: "double",
-                        },
-                      },
-                    ],
-                  },
                 },
               ],
             },
           ],
           default: null,
         },
+      ],
+    },
+  },
+};
+
+export const lsst_crossmatch_fields = {
+  name: "cross_matches",
+  type: {
+    type: "array",
+    items: {
+      type: "record",
+      name: "CrossMatch",
+      fields: [
         {
           name: "NED",
           type: [
@@ -457,28 +565,12 @@ export const lsst_crossmatch_fields = {
                   type: "string",
                 },
                 {
-                  name: "coordinates",
-                  type: {
-                    type: "record",
-                    name: "Coordinates",
-                    fields: [
-                      {
-                        name: "radec_str",
-                        type: {
-                          type: "array",
-                          items: "string",
-                        },
-                      },
-                      {
-                        name: "distance_arcsec",
-                        type: "double",
-                      },
-                      {
-                        name: "distance_kpc",
-                        type: "double",
-                      },
-                    ],
-                  },
+                  name: "Mstar",
+                  type: "float",
+                },
+                {
+                  name: "Mstar_unc",
+                  type: "float",
                 },
               ],
             },
@@ -486,52 +578,32 @@ export const lsst_crossmatch_fields = {
           default: null,
         },
         {
-          name: "Gaia_DR3",
+          name: "LSPSC",
           type: [
             "null",
             {
               type: "record",
-              name: "Gaia_EDR3Match",
+              name: "LSPSCMatch",
               fields: [
                 {
                   name: "_id",
                   type: "double",
                 },
                 {
-                  name: "parallax",
+                  name: "ra",
                   type: "double",
                 },
                 {
-                  name: "parallax_error",
+                  name: "dec",
                   type: "double",
                 },
                 {
-                  name: "phot_g_mean_mag",
-                  type: "double",
+                  name: "score",
+                  type: "float",
                 },
                 {
-                  name: "phot_bp_mean_mag",
-                  type: "double",
-                },
-                {
-                  name: "phot_rp_mean_mag",
-                  type: "double",
-                },
-                {
-                  name: "coordinates",
-                  type: {
-                    type: "record",
-                    name: "Coordinates",
-                    fields: [
-                      {
-                        name: "radec_str",
-                        type: {
-                          type: "array",
-                          items: "string",
-                        },
-                      },
-                    ],
-                  },
+                  name: "mag_white",
+                  type: "float",
                 },
               ],
             },
