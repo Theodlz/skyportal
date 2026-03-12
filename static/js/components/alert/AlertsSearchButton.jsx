faf0d473921c39d2d50ce50f63f4e8cee1c75514
Theodlz <theophile.dulaz@gmail.com>
2026-03-12 13:32:43 +0100

    add more fritz components to this branch

---
+++ static/js/components/alert/AlertsSearchButton.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const AlertsSearchButton = ({ ra, dec, radius = 3 }) => {
+  return (
+    <Link
+      to={`/alerts?ra=${ra}&dec=${dec}&radius=${radius}&group_by_obj=true`}
+      target="_blank"
+      style={{ textDecoration: "none", color: "black" }}
+    >
+      ZTF Alerts
+    </Link>
+  );
+};
+
+AlertsSearchButton.propTypes = {
+  ra: PropTypes.number.isRequired,
+  dec: PropTypes.number.isRequired,
+  radius: PropTypes.number,
+};
+AlertsSearchButton.defaultProps = {
+  radius: 3,
+};
+
+export default AlertsSearchButton;
